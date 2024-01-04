671e6ce77df365128833242d3554ca7bf76b7b60
VeronicaG24 <veronicagavagna.vg@gmail.com>
2024-01-04 15:04:46 +0000

    bug fix

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/root/ros_ws/src/Assignment2_exp/devel:$CMAKE_PREFIX_PATH"
+export PWD='/root/ros_ws/src/Assignment2_exp/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/root/ros_ws/src/Assignment2_exp/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/root/ros_ws/src/Assignment2_exp/src:$ROS_PACKAGE_PATH"
